951c8febcae0bed323f7074a49dd64bebe26c5d9
Rui Chen <rui@chenrui.dev>
2021-08-30 20:56:34 -0400

    e2e: improve test
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -2,6 +2,10 @@ import { test, expect } from "@playwright/test";
 
 test("basic test", async ({ page }) => {
   await page.goto("http://localhost:3000/");
-  const title = page.locator("h2");
-  await expect(title).toHaveText("config test");
+
+  const h1 = page.locator("h1");
+  await expect(h1).toHaveText("Welcome to Next.js!");
+
+  const h2 = page.locator("h2");
+  await expect(h2).toHaveText("config test");
 });
