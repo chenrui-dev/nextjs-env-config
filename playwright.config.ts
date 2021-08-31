e83aab8b95f3bed558f282e23f908aa3538059bd
Rui Chen <rui@chenrui.dev>
2021-08-30 20:46:32 -0400

    e2e: simple spec test

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,12 @@ import { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   name: "Chrome Stable",
   testDir: "e2e",
+  webServer: {
+    command: "yarn dev:test",
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   use: {
     browserName: "chromium",
     // Test against Chrome Stable channel.
